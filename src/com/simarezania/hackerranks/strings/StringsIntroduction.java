751a4b1cb6e745e15383e2278031fb6974e5aebc
simarezania <sima.rezania@gmail.com>
2017-06-08 13:58:15 +0200

    Some Strings methods

---
+++ src/com/simarezania/hackerranks/strings/StringsIntroduction.java
@@ -0,0 +1,21 @@
+package com.simarezania.hackerranks.strings;
+
+import java.util.Scanner;
+
+public class StringsIntroduction {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String B = sc.next();
+		System.out.println(A.length() + B.length());
+		if (A.compareTo(B)>0) System.out.println("Yes");
+		else System.out.println("No");
+		System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1)+" " + B.substring(0, 1).toUpperCase()+B.substring(1));
+		
+		// Scanner.close;
+		/* Enter your code here. Print output to STDOUT. */
+
+	}
+
+}
