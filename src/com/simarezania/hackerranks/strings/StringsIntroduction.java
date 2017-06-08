93a5767e6eaacc922e8a6dd30049d868685b8186
simarezania <sima.rezania@gmail.com>
2017-06-08 14:01:28 +0200

    modified previous one

--- src/com/simarezania/hackerranks/strings/StringsIntroduction.java
+++ src/com/simarezania/hackerranks/strings/StringsIntroduction.java
@@ -9,10 +9,16 @@ public class StringsIntroduction {
 		String A = sc.next();
 		String B = sc.next();
 		System.out.println(A.length() + B.length());
-		if (A.compareTo(B)>0) System.out.println("Yes");
-		else System.out.println("No");
-		System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1)+" " + B.substring(0, 1).toUpperCase()+B.substring(1));
-		
+		if (A.compareTo(B) > 0)
+			System.out.println("Yes");
+
+		else
+			System.out.println("No");
+		System.out.println(A.compareTo(B));
+		System.out.println(B.compareTo(A));
+		System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase()
+				+ B.substring(1));
+
 		// Scanner.close;
 		/* Enter your code here. Print output to STDOUT. */
 
